92e7e0505f297e4363f4d22e3092c97353f64627
Eduard Baescu <eduard@geev.com>
2026-05-06 11:02:34 +0200

    feat/Export_to_csv - GREEN

--- src/export-csv.js
+++ src/export-csv.js
@@ -1,3 +1,5 @@
-export function transactionsToCsvHeader(data) {
-  return data;
+export function transactionsToCsvHeader(rows) {
+  if (!rows.length) return '';
+  const headers = Object.keys(rows[0]);
+  return headers.join(',');
 }
